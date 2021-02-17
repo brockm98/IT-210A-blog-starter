fda07ed3d172d64856da88725ae7f3ddf8d13936
Brock May <brockcm98@gmail.com>
2021-02-17 11:48:04 -0700

    Template

---
+++ src/index.php
@@ -0,0 +1,18 @@
+<?php 
+    // TODO: This starts our PHP session for us
+    // TODO: Make SQL queries
+    // TODO: Build HTML for carosuel
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Photo Carousel</title>
+</head>
+<body>
+    <p>If you are seeing this, the docker container works!</p>
+</body>
+</html>
